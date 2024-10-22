a5a4c190756901657c415e2f78fcefd0c44e0471
SAÏBI Yacine <yacine.saibi@orange.com>
2024-10-22 11:11:28 +0200

    first commit

---
+++ custom_tests/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.3.9"
+}
+
+
+resource "kubernetes_secret" "example" {
+  metadata {
+    name = "basic-auth"
+  }
+
+  data = {
+    username = "admin"
+    password = "P4ssw0rd"
+  }
+
+  type = "kubernetes.io/basic-auth"
+}
