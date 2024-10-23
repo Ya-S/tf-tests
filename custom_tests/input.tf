4d79edf98069d86ba32fabc680bec19cf5fcfdc3
SAÏBI Yacine <yacine.saibi@orange.com>
2024-10-23 14:47:13 +0200

    update resources

---
+++ custom_tests/input.tf
@@ -0,0 +1,11 @@
+variable "namespace" {
+   type = string
+   default = "ys-helloworld"
+   description = "Target namespace"
+}
+
+variable "username" {
+   type = string
+   default = "admin"
+   description = "username"
+}
--- custom_tests/main.tf
+++ custom_tests/main.tf
@@ -2,15 +2,25 @@ terraform {
   required_version = ">= 1.3.9"
 }
 
+resource "random_password" "password" {
+  length           = 32
+  min_lower        = 5
+  min_numeric      = 5
+  min_special      = 5
+  min_upper        = 5
+  special          = true
+  override_special = "!#%&*()-_=+[]{}<>:?"
+}
 
 resource "kubernetes_secret" "example" {
   metadata {
-    name = "basic-auth"
+    name      = "basic-auth"
+    namespace = "var.namespace"
   }
 
   data = {
-    username = "admin"
-    password = "P4ssw0rd"
+    username = "var.username"
+    password = resource.random_password.password.result
   }
 
   type = "generic"
